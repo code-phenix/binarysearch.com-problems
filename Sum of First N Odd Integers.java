41ccd8d4c61d863d9ca94672cfdf2aed03ceb1bb
codepheonix <55018311+code-phenix@users.noreply.github.com>
2021-08-24 14:52:05 +0530

    Create Sum of First N Odd Integers.java

---
+++ Sum of First N Odd Integers.java
@@ -0,0 +1,21 @@
+// Given an integer n, return the sum of the first n positive odd integers.
+
+// Constraints
+
+// n ≤ 1,000
+// Example 1
+// Input
+// n = 5
+// Output
+// 25
+// Explanation
+// The first 5 odd integers are [1, 3, 5, 7, 9] and its sum is 25.
+
+class Solution {
+    
+    public int solve(int n) {
+        
+        return n*n;
+    }
+}
+// Your code took 4 milliseconds — faster than 99.96% in Java
